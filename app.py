ce31d2d7b0dc23abc8915d74b394a0dc9530fac7
Joy Ukpabi <joyucheharry@gmail.com>
2025-11-28 17:36:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Python Docker App</h1><p>Status endpoint: /status</p>"
+    return "<h1>MY DEVOPS SIMPLE PYTHON APP</h1><p>Status endpoint: /status</p>"
 
 @app.route("/status")
 def status():
     return jsonify({
         "status": "running",
         "time": datetime.utcnow().isoformat() + "Z",
-        "message": "Hello from the containerized Python app!"
+        "message": "Hello from the JOYCLOUD SOLUTIONS!"
     })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
